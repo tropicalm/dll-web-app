bea778f205ea7286712117651e70cbdd9802723e
Mathias Åsberg <mathias.asberg@gmail.com>
2020-02-14 05:03:43 +0100

    auth function OK

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import withFirebaseAuth from "react-with-firebase-auth";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "./firebaseConfig";
+import logo from "./logo.svg";
+import "./App.css";
 
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-function App() {
+// Configure FirebaseUI.
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: "popup",
+  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: "/signedIn",
+  // We will display Google and Facebook as auth providers.
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
+  ]
+};
+
+class App extends Component {
+  render() {
+    const { user, signOut, signInWithGoogle } = this.props;
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-        </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-        </a>
-            </header>
-        </div>
+      <div className="App">
+        <header className="App-header">
+          <StyledFirebaseAuth
+            uiConfig={uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        </header>
+      </div>
     );
+  }
 }
 
-export default App;
+const firebaseAppAuth = firebaseApp.auth();
+
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider()
+};
+
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth
+})(App);
---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyARxGoTayGEwEdO-3ZoGysPdp54XZ10YZo",
+  authDomain: "nextgrid-254813.firebaseapp.com",
+  databaseURL: "https://nextgrid-254813.firebaseio.com",
+  projectId: "nextgrid-254813",
+  storageBucket: "nextgrid-254813.appspot.com",
+  messagingSenderId: "78431916316",
+  appId: "1:78431916316:web:e673c728205d001ed66ad9",
+  measurementId: "G-YT88V5HK8K"
+};
